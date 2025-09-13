1829801a983cde24c43ddd7f0b634984f1251cd0
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2025-09-13 14:05:42 +0530

    Create Vector-Sort.cpp

---
+++ Vector-Sort.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+     int n, x;
+    cin >> n;
+    vector<int> v;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; ++i) {
+        cout << v[i] << " ";
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
