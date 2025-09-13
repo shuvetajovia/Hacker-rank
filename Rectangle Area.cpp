080cd7d9033d9d332deed643e3f19058d8b7003b
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2025-09-13 13:41:45 +0530

    Create Rectangle Area.cpp

---
+++ Rectangle Area.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    protected:
+        int width, height;
+    public:
+        void display() {
+            cout << width << " " << height << endl;
+        }
+};
+
+class RectangleArea : public Rectangle {
+    public:
+        void read_input() {
+            cin >> width >> height;
+        }
+        
+        void display() {
+            cout << width * height << endl;
+        }
+};
+
+int main() {
+    RectangleArea r_area;
+
+    r_area.read_input();
+    r_area.Rectangle::display(); // Call base class display (width and height)
+    r_area.display();            // Call derived class display (area)
+
+    return 0;
+}
