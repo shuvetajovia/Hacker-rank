d65b911badb617a7f452acf356aa086135433648
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2025-09-13 14:30:45 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+     string a;
+    string b;
+    string c;
+    string d;
+    string e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
